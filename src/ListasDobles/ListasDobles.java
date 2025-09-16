3ea387ed02e41a2a25cec194bffa77c48ac5032d
CENTIC <CENTIC@10.6.66.139>
2025-09-16 11:28:31 -0500

    clase nueva de main

--- src/ListasDobles/ListasDobles.java
+++ src/ListasDobles/ListasDobles.java
@@ -10,13 +10,27 @@ public class ListasDobles {
         
         lista.insertarNodoInicio(3);
         lista.insetarNodoFin(4);
-        lista.imprimirListaIniAFin();
-        lista.eliminarNodoInicio();
+        //lista.imprimirListaIniAFin();
+        //lista.eliminarNodoInicio();
         
+        /*
         System.out.println("Despuede haber borrado ");
         
         lista.imprimirListaFinAInicio();
         lista.imprimirListaIniAFin();
+        */
+        System.out.println("Eliminar inidice ");
+        
+        lista.insertarNodoInidce( 6,1);
+        lista.imprimirListaFinAInicio();
+        lista.imprimirListaIniAFin();
+        System.out.println("tamaño" + lista.getTamaño());
+        
+        lista.eliminarNodoIndice(6, 1);
+        System.out.println("Dato eliminado");
+        lista.imprimirListaIniAFin();
+        lista.imprimirListaFinAInicio();
+        
     }
     
     
