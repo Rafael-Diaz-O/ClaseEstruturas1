644f84826d820e25fe399c4533e6851e1a56063b
CENTIC <CENTIC@10.6.66.139>
2025-09-23 10:56:59 -0500

    fgfbdfdbr

---
+++ src/Arboles/Nodo.java
@@ -0,0 +1,17 @@
+
+package Arboles;
+
+
+public class Nodo {
+    
+    public int dato;
+    public Nodo siguiente; 
+    
+    public Nodo(int dato){
+        this.dato = dato;
+        this.siguiente = null;
+        
+    }
+    
+    
+}
