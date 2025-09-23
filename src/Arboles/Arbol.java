69e28b0bfd223da7159085d1e6e50ad29755463d
CENTIC <CENTIC@10.6.66.139>
2025-09-23 11:04:32 -0500

    eafwefwef

--- src/Arboles/Arbol.java
+++ src/Arboles/Arbol.java
@@ -4,9 +4,30 @@ package Arboles;
 
 public class Arbol {
     
-    private int raiz;
+    private Nodo raiz;
     private int tamaño;
     
+    public Arbol(){
+        this.tamaño = 0;
+        this.raiz = null;
+    }
     
+    //verifica si el arbol esta vacio y sele avisa al usuario
+    public void vacio(){
+        if (raiz == null){
+            System.out.println("el arbol esta vacio");
+        }else{
+            System.out.println("el arbol no esta vacio");
+        }
+    }
+    //verificacion en segundo plano que solo corra si es verdadero o falso pero no sele indique al usuario
+    
+    public boolean estaVacio(){
+        if(raiz== null){
+            return true;
+        }else{
+            return false; 
+        }
+    }
     
 }
