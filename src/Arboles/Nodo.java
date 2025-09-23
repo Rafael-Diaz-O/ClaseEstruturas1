b9148fa8eb54d59728dd39f5b1afaac1b49b52b1
CENTIC <CENTIC@10.6.66.139>
2025-09-23 11:04:46 -0500

    efewfwe

--- src/Arboles/Nodo.java
+++ src/Arboles/Nodo.java
@@ -6,10 +6,15 @@ public class Nodo {
     
     public int dato;
     public Nodo siguiente; 
+    public Nodo hijoIzquierdo;
+    public Nodo hijoDerecho;
+    
     
     public Nodo(int dato){
         this.dato = dato;
         this.siguiente = null;
+        this.hijoDerecho = null;
+        this.hijoIzquierdo = null;
         
     }
     
