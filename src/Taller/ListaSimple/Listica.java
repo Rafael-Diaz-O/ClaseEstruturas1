e081aa6cc1ef67ad9caed72acf7a8d89c2a29aca
Rafael <rafadz@gmail.com>
2025-10-05 21:18:45 -0500

    avances

--- src/Taller/ListaSimple/Listica.java
+++ src/Taller/ListaSimple/Listica.java
@@ -8,11 +8,17 @@ public class Listica {
         
         Lista1 numeros = new Lista1();
         
-        numeros.insertarNodoInicio(3);
-        numeros.insertarNodoFinal(6);
-        numeros.insertarNodoInicio(3);
-        numeros.insertarNodoInicio(3);
+        numeros.insertarNodoInicio(6);
+        numeros.insertarNodoFinal(9);
+        numeros.insertarNodoInicio(6);
+        numeros.insertarNodoInicio(1);
+        numeros.insertarNodoInicio(1);
+      
+        
+        System.out.println("Lista: ");
         numeros.imprimirLista();
+        
+        System.out.println("Lista sin elementos repetidos: ");
         numeros.eliminarDuplicados();
         numeros.imprimirLista();
     }
