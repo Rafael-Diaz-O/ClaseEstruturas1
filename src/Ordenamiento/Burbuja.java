aaa6fc9fdf076ffdeb70f017d71ee8ce5aba0f7f
CENTIC <CENTIC@10.6.66.139>
2025-10-28 11:52:30 -0500

    dfhdtht

--- src/Ordenamiento/Burbuja.java
+++ src/Ordenamiento/Burbuja.java
@@ -21,16 +21,66 @@ public class Burbuja {
    
    public void imprimirVector(int [] vector){
        for(int i= 0; i < vector.length; i++){
-       System.out.println(vector[i] + "");
+       System.out.print("{" + vector[i] + "}");
    }
        
        System.out.println();
    }
    
    
-
+public void seleccion(int[] vector){
+    
+    int temp;
+    int pos_min_actual;
+    
+    for(int i = 0; i < vector.length -1; i ++ ){
+        pos_min_actual = i;
+        
+        for(int j = i+1; j < vector.length; j++){
+        if(vector[j]< vector[pos_min_actual]){ // estoy en la posicon comparando valores 
+            pos_min_actual = j;
+        }
+        
+        if(vector[pos_min_actual] < vector[i]){ //compara los dos
+            temp = vector[i]; 
+            vector[i] = vector[pos_min_actual];
+            vector[pos_min_actual]= temp;// queda con un valor mayor en esa posicion
+        }
+    }
+    }
+    
+}
     
     
+   public void insecion(int[] vector){
+       int temp;
+       int pos_min_actual;
+       
+       for(int i = 0; i < vector.length; i ++){
+           pos_min_actual =  i; 
+           
+            for(int j = i+1; j<vector.length; j++ ){
+                if(vector[j]<vector[pos_min_actual]){
+                   pos_min_actual = j; 
+                }
+            }
+           
+            if(vector[pos_min_actual] < vector[i]){
+                
+            }
+           
+           
+       }
+       
+   }
+   
+   public void insercion(int[]vector){
+       for(int i = 1; i < vector.length; i++){
+           int temp = vector[i];
+           int hueco = i;
+           while (hueco )
+       }
+   }
     
     
 }
